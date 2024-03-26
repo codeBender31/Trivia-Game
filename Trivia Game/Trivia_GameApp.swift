b0fdfad7e3e11a587728edbc447eb9dba267af36
codeBender31 <ibarraisrael949@yahoo.com>
2024-03-26 14:09:40 -0500

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Bender on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
